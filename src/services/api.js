9dafa097b8f2693b6794127411ef90fb840f8553
Vinícius Carvalho <vini.cius@uol.com.br>
2024-05-15 13:36:23 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -2,9 +2,7 @@ import axios from "axios";
 
 export const api = axios.create({
     // prod url:
-    baseURL: "https://rocket-movies-backend-w78i.onrender.com
-
-    ",
+    baseURL: "https://rocket-movies-backend-w78i.onrender.com",
 
     // dev url:
     //baseURL: "http://localhost:3333", 
